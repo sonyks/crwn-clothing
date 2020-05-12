bab10226f94b6bb0fd1116f4144ac5bbfe399383
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-05-13 01:47:53 +0300

    add collection-item and preview component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import './App.css';
-import HomePage from '../src/pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import { Switch, Route } from 'react-router-dom';
-
-const TopicList = (props) => (
-  <div>
-    <h1>TOPIC LIST PAGE</h1>
-  </div>
-);
-
-const TopicDetail = (props) => (
-  <div>
-    <h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>
-  </div>
-);
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/topics' component={TopicList}/>
-        <Route exact path='/topics/:topicId' component={TopicDetail}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
