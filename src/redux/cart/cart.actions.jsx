f3dd7361145d00cf49c83e8b4207caaaec178f8f
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-05-27 00:19:02 +0300

    add cart item and actions

--- src/redux/cart/cart.actions.jsx
+++ src/redux/cart/cart.actions.jsx
@@ -3,3 +3,8 @@ import CardActuionTypes from './cart.types';
 export const toggleCartHidden = () => ({
     type: CardActuionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => ({
+    type: CardActuionTypes.ADD_ITEM,
+    payload: item
+});
