9e7226709f10e4c1e5a42aa6bedf75c3b753a5f5
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-06-03 02:11:53 +0300

    added checkout component

--- src/redux/cart/cart.selectors.jsx
+++ src/redux/cart/cart.selectors.jsx
@@ -10,9 +10,14 @@ export const selectCartItems = createSelector(
 export const selectCartHidden = createSelector(
     [selectCart],
     (cart) => cart.hidden
-)
+);
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     (cartItems) => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
 )
