5d112dbb3f43130e9057e1f6742741b0b796c1fe
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-06-04 01:13:12 +0300

    updated cart actions and reducer

--- src/redux/cart/cart.actions.jsx
+++ src/redux/cart/cart.actions.jsx
@@ -8,3 +8,14 @@ export const addItem = item => ({
     type: CardActuionTypes.ADD_ITEM,
     payload: item
 });
+
+export const removeItem = item => ({
+    type: CardActuionTypes.REMOVE_ITEM,
+    payload: item
+});
+
+export const clearItemFromCart = item => ({
+    type: CardActuionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
+
