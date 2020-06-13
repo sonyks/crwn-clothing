b698e124a78b4e4532495d502e23924ded839dce
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-06-14 02:03:01 +0300

    added styled components to project

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import {createStructuredSelector} from 'reselect';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import { connect } from 'react-redux';
@@ -10,33 +9,33 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { auth } from '../../firebase/firebase.utils';
 
-import './header.styles.scss'
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles'
 
 const Header = ({currentUser, hidden}) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser ?
-                <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>
+                <OptionDiv onClick={ () => auth.signOut() }>SIGN OUT</OptionDiv>
                 :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                <OptionLink to='/signin'>SIGN IN</OptionLink>
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {
             hidden ? null :
             <CartDropdown />
         }     
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
