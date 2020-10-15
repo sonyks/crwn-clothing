2b5df5de7a886a1b7c0c4ddd7bb5b762da1ab83d
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-10-16 01:42:57 +0300

    refactored singin with sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,8 +3,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomComponent from '../custom-button/custom-button.component';
 import { connect } from 'react-redux';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
-import { googleSignInStart } from '../../redux/user/user.actions';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -20,17 +19,10 @@ class SignIn extends React.Component {
     handleSubmit = async event => {
         event.preventDefault();
 
-        const {email, password} = this.state;
+        const { emailSignInStart } = this.props;
+        const { email, password } = this.state;
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({
-                email: '',
-                password: ''
-            });
-        } catch (error) {
-            console.log(error);
-        }  
+        emailSignInStart(email, password);
     }
 
     handleChange = event => {
@@ -73,6 +65,7 @@ class SignIn extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-    googleSignInStart: () => dispatch(googleSignInStart())
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({email, password}))
 })
 export default connect(null, mapDispatchToProps)(SignIn);
