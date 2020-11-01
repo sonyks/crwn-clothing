e86a1c54bbf855da1ecb5fcbb99a99d8226dc095
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-11-01 14:06:25 +0200

    update coe to be PWA compliant

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,3 +20,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
